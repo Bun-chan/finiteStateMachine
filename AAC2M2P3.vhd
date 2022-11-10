c2dedfe09c6300afc88b25773d5026b93ab25b97
unknown <stephenjamesfitzgerald@yahoo.com>
2022-11-10 18:56:08 +0900

    finished the first process. started the 2nd process

--- AAC2M2P3.vhd
+++ AAC2M2P3.vhd
@@ -7,3 +7,19 @@ port (In1: in std_logic;
    CLK: in std_logic;
    Out1 : inout std_logic);
 end FSM;
+architecture fsm_arch of FSM is
+	type state_type is (a, b, c);
+	signal current_state, next_state : state_type;
+begin
+	state_memory_process : process (CLK, RST)
+		begin
+			if (RST = '1') then
+				current_state <= a; --what is the expected action when reset is applied? I just assumed reset to state 'a'.
+			elsif (rising_edge(CLK)) then 
+				current_state <= next_state;
+			end if; --if RST=1
+		end process;
+	state_logic_process : process (current_state, In1)
+		begin
+		end process;
+end fsm_arch;
